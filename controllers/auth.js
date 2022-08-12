35b97eb4e758c13609c972df5ed06cb323ae3640
Abiola Olamilekan <abiolawalter2015@gmail.com>
2022-08-13 00:20:20 +0100

    implemented the controllers and the model schema

---
+++ controllers/auth.js
@@ -0,0 +1,5 @@
+exports.create_or_update_user = (req, res) => {
+    res.json({
+        data: "The create_or_update_user API endpoint",
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,14 @@
 // Import Statements
 const express = require("express");
 
+// Controller Import
+const { create_or_update_user } = require("../controllers/auth");
+
 // Router variables
 const router = express.Router();
 
 // Endpoints
-router.get("/create_or_update_user", (req, res) => {
-    res.json({
-        data: "The create_or_update_user API endpoint",
-    });
-});
+router.get("/create_or_update_user", create_or_update_user);
 
 // Export Statements
 module.exports = router;
